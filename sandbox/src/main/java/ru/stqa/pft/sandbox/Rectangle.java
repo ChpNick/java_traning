41dc668ed4843af8a3927aa7d030b97374323cbe
Pechenin.NA <chpnick1@gmail.com>
2017-04-16 17:13:50 +0300

    New file and class added

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Nikolay Pechenin on 16.04.2017.
+ */
+public class Rectangle {
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double area() {
+        return this.a * this.b;
+    }
+}
