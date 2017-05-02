0b379af8ca5dfae79792fcbdf75fa951ff54fc15
Pechenin.NA <chpnick1@gmail.com>
2017-05-02 22:39:57 +0300

    Переименовал метод похожий на конструктор

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -24,7 +24,7 @@ public class GroupCreationTests {
     }
     
     @Test
-    public void GroupCreationTests() {
+    public void testGroupCreation() {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
