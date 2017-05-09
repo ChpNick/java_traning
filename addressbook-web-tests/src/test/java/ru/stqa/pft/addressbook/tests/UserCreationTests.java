9bbc90a8aed9ee875b8b5da0585bf29a2459e888
Pechenin.NA <chpnick1@gmail.com>
2017-05-09 22:11:15 +0300

    Добавил делегирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
@@ -1,17 +1,18 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.UserData;
 
 public class UserCreationTests extends TestBase {
 
     @Test
     public void testUserCreation() {
-        gotoAddNewUserPage();
-        fillAddNewUserForm(new UserData("test", "testovich", "testov", "Chp", "i like coding", "social quantum",
+        app.gotoAddNewUserPage();
+        app.fillAddNewUserForm(new UserData("test", "testovich", "testov", "Chp", "i like coding", "social quantum",
                 "Ivanovo", "888888", "89234567890", "12345678900", "-", "chpnick@mail.ru", "chpnick1@gmail.ru",
                 "-", "-", 1, 2, "1983", 1, 2, "1990", "-", "-", "-"));
-        submitNewUserCreation();
-        gotoHomePage();
+        app.submitNewUserCreation();
+        app.gotoHomePage();
     }
 
 }
