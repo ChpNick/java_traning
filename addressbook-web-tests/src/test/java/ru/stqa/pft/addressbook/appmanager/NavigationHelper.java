eb320a53e75716b47cf9e418573f491d925f8387
Pechenin.NA <chpnick1@gmail.com>
2017-05-18 00:31:00 +0300

    Добавил проверку, чтоб не делался повторный переход по локатору

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -7,21 +7,34 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Nikolay Pechenin on 10.05.2017.
  */
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
     public void gotoGroupPage() {
+        if (isElemebtPresent(By.xpath(".//*[@id=\"content\"]/h1"))
+                && wd.findElement(By.xpath(".//*[@id=\"content\"]/h1")).getText().equals("Groups")
+                && isElemebtPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void gotoHomePage() {
+        if (isElemebtPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
 
     public void gotoAddNewContactPage() {
+        if (isElemebtPresent(By.xpath(".//*[@id='content']/h1"))
+                && wd.findElement(By.xpath(".//*[@id='content']/h1")).getText().equals("Edit / add address book entry")
+                && isElemebtPresent(By.name("submit"))) {
+            return;
+        }
         click(By.linkText("add new"));
     }
 }
