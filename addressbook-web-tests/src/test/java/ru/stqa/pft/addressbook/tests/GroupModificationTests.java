13c40f2b4474cd1c7f5efd8ba406946bda5c47a1
Pechenin.NA <chpnick1@gmail.com>
2017-05-24 23:52:07 +0300

    добавил проверки и на множества и на списки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -34,10 +35,18 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
+//        Проверяем через множества
         before.remove(before.size() - 1);
         before.add(group);
 
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+//        Проверяем по отсортированным спискам
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+
+        Assert.assertEquals(before, after);
 
     }
 }
