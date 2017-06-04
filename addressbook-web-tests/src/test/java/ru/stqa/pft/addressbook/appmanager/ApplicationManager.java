f9595cc1ff000398714c4babc509f5bd2d5f23ef
Pechenin.NA <chpnick1@gmail.com>
2017-06-04 22:26:46 +0300

    Добавил конфигурационные файлы и переделал работу через них

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,12 +7,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Nikolay Pechenin on 09.05.2017.
  */
 public class ApplicationManager {
+    private final Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -23,9 +29,12 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
@@ -44,8 +53,8 @@ public class ApplicationManager {
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
 
-        wd.get("http://localhost/addressbook/");
-        sessionHelper.login("admin", "secret");
+        wd.get(properties.getProperty("web.baseUrl"));
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.common.collect.ForwardingSet;
-import com.sun.org.apache.bcel.internal.generic.NEW;
 
 import java.util.HashSet;
 import java.util.Set;
