9a8d826db1988d0585a286f20e09e5c6814fb443
Pechenin.NA <chpnick1@gmail.com>
2017-06-13 23:32:21 +0300

    Add new helper

--- mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -32,6 +32,7 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
+        //noinspection Duplicates
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -49,6 +50,10 @@ public class ApplicationManager {
 
     public void stop() {
         wd.quit();
+    }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
     }
 
 }
---
+++ mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.mantis.appmanager;
+
+/**
+ * Created by Nikolay Pechenin on 13.06.2017.
+ */
+public class HttpSession {
+    private final ApplicationManager app;
+
+    public HttpSession(ApplicationManager app) {
+        this.app = app;
+    }
+}
